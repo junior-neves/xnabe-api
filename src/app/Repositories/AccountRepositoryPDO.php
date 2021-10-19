5a8dbe6ef2c602749a86f0045705cdd33e119eb6
Junior Neves <junior.neves@outlook.com>
2021-10-19 02:39:59 -0300

    Updating AccountRepository

--- src/app/Repositories/AccountRepositoryPDO.php
+++ src/app/Repositories/AccountRepositoryPDO.php
@@ -13,26 +13,32 @@ class AccountRepositoryPDO implements AccountRepositoryInterface
     {
         $this->pdo = Database::getInstance();
     }
-
-    public function create($account_id, $initial_balance)
+//TODO: retirar o rs
+//TODO: mudar variaveis para camel case
+    public function create($account_id, $initial_balance) : bool
     {
         $rs = $this->pdo->prepare("INSERT INTO account (id, balance) VALUES (?, ?)");
         $rs = $rs->execute([$account_id, $initial_balance]);
         return $rs;
     }
 
-    public function updateBalance($account_id, $balance)
+    public function updateBalance($account_id, $balance) : bool
     {
         $rs = $this->pdo->prepare("UPDATE account SET balance = ? WHERE id = ?");
         $rs = $rs->execute([$balance, $account_id]);
         return $rs;
     }
 
-    public function getOne($account_id)
+    public function getOne($account_id) : ?array
     {
         $rs = $this->pdo->prepare("SELECT * FROM account WHERE id = ?");
         $rs->execute([$account_id]);
-        return $rs->fetch();
+        $rs = $rs->fetch();
+        if (!$rs) {
+            return null;
+        }
+        $rs["balance"] = (int)($rs["balance"]);
+        return $rs;
     }
 
     public function getBalance($account_id) : int
--- src/app/Repositories/Contracts/AccountRepositoryInterface.php
+++ src/app/Repositories/Contracts/AccountRepositoryInterface.php
@@ -4,6 +4,12 @@ namespace App\Repositories\Contracts;
 
 interface AccountRepositoryInterface
 {
-    public function create($account_id, $initial_balance);
+    public function create($account_id, $initial_balance) : bool;
+
+    public function updateBalance($account_id, $balance) : bool;
+
+    public function getOne($account_id) : ?array;
+
+    public function getBalance($account_id): int;
 
 }
